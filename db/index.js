fb627deed0d5048bb1d29f82e58422aa25569bca
Peter McBride <mrpetermcbride@gmail.com>
2022-07-29 20:02:33 -0700

    Fixed server connection issue, manual inputing database name in route.js

--- db/index.js
+++ db/index.js
@@ -3,21 +3,29 @@ require('dotenv').config();
 
 const pool = new Pool({
   user: process.env.USERNAME || '',
-  database: process.env.DBNAME,
-  host: '127.0.0.1',
+  database: 'questions',
+  host: 'localhost',
   password: process.env.PASSWORD,
   port: 5432,
 })
 
 module.exports = {
   getQuestions: (productId, page, count) => {
-    let queryString = `SELECT * FROM questions LIMIT ${limit}`;
+    let queryString =
+    // `SELECT *
+    // FROM questions
+    // WHERE product_id = ${productId}
+    // ORDER BY DESC
+    // LIMIT ${count || page}`;
+    `SELECT * FROM questions WHERE product_id = ${productId} AND reported = false`;
     return pool.query(queryString)
-      .then(res => {console.log('query all questions', res)})
+      .then(res => { console.log('get all questions', res) })
       .catch(err => console.log('error getting all questions', err))
-      // const duration = Date.now() - start
+    // const duration = Date.now() - start
   },
 
-  // One above would be like getQuestions
-}
+  getAnswers: (questionId, page, count) => {
+    console.log('working on it')
 
+  },
+}
