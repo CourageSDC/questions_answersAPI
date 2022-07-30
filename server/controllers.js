389c7b94e5c1bc2782a788fdd5f57053e930b5bb
Peter McBride <mrpetermcbride@gmail.com>
2022-07-29 23:32:19 -0700

    Edited routes.js and controllers.js

--- server/controllers.js
+++ server/controllers.js
@@ -9,7 +9,7 @@ module.exports = {
     let count = req.query.count || 5;
     db.getQuestions(productId, page, count)
       .then(data => res.status(200).send(data))
-      .catch(err => res.status(400))
+      .catch(err => res.sendStatus(400))
   },
 
   getAnswers: (req, res) => {
@@ -18,33 +18,50 @@ module.exports = {
     let count = req.query.count || 5;
     db.getAnswers(questionId, page, count)
       .then(data => res.status(200).send(data))
-      .catch(err => res.status(400))
+      .catch(err => res.sendStatus(400))
   },
 
   postQuestion: (req, res) => {
-    let productId = req.query.product_id;
+    // let productId = req.query.product_id;
     let formInput = req.query.body;
     db.postQuestion (productId, formInput)
-      // .then(data => res.)
+      .then(response => res.sendStatus(201))
+      .catch(err => res.sendStatus(400))
   },
 
   postAnswer: (req, res) => {
-
+    let questionId = req.params.question_id;
+    let formInput = req.query.body;
+    db.postAnswer (questionId, formInput)
+      .then(response => res.sendStatus(201))
+      .catch(err => res.sendStatus(400))
   },
 
   incrementQuestionHelp: (req, res) => {
-
+    let questionId = req.params.question_id;
+    db.incrementQuestionHelp (questionId)
+      .then(response => res.sendStatus(204))
+      .catch(err => res.sendStatus(400))
   },
 
   reportQuestion: (req, res) => {
-
+    let questionId = req.params.question_id;
+    db.reportQuestion (questionId)
+      .then(response => res.sendStatus(204))
+      .catch(err => res.sendStatus(400))
   },
 
   incrementAnswerHelp: (req, res) => {
-
+    let answerId = req.params.answer_id;
+    db.incrementAnswerHelp (answerId)
+      .then(response => res.sendStatus(204))
+      .catch(err => res.sendStatus(400))
   },
 
   reportAnswer: (req, res) => {
-
+    let answerId = req.params.answer_id;
+    db.reportAnswer (answerId)
+      .then(response => res.sendStatus(204))
+      .catch(err => res.sendStatus(400))
   }
 }
--- server/routes.js
+++ server/routes.js
@@ -15,40 +15,22 @@ router.get('', controller.getQuestions)
 router.get('/:question_id/answers', controller.getAnswers)
 
 // Post a Question
-router
-  .route('/')
-  .post(controller.postQuestion)
-  // return 201 Created
+router.post('', controller.postQuestion)
 
 // Post an Answer
-router
-  .route('/:question_id/answers')
-  .post(controller.postAnswer)
-  // return 201 Created
+router.post('/:question_id/answers', controller.postAnswer)
 
 //Mark Question as Helpful
-router
-  .route('/:question_id/helpful')
-  .put(controller.incrementQuestionHelp)
-  // return 204 No Content
+router.put('/:question_id/helpful', controller.incrementQuestionHelp)
 
 //Report Question
-router
-  .route('/:question_id/report')
-  .put(controller.reportQuestion)
-  //return 204 No Content
+router.put('/:question_id/report', controller.reportQuestion)
 
 //Mark Answer as Helpful
-router
-  .route('/:question_id/helpful')
-  .put(controller.incrementAnswerHelp)
-  // return 204 No Content
+router.put('/:question_id/helpful', controller.incrementAnswerHelp)
 
 //Report Answer
-router
-  .route('/:question_id/report')
-  .put(controller.reportAnswer)
-  // return 204 No Content
+router.put('/:question_id/report', controller.reportAnswer)
 
 module.exports = router
 
