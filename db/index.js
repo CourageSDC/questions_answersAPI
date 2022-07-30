54d1d7fb517f078c79f3eb08b6bae5ba15be67ee
Peter McBride <mrpetermcbride@gmail.com>
2022-07-29 21:28:49 -0700

    Added csv files to repo

--- db/index.js
+++ db/index.js
@@ -12,14 +12,14 @@ const pool = new Pool({
 module.exports = {
   getQuestions: (productId, page, count) => {
     let queryString =
+    `SELECT * FROM questions WHERE product_id =40344 AND reported IS FALSE`;
     // `SELECT *
     // FROM questions
     // WHERE product_id = ${productId}
     // ORDER BY DESC
     // LIMIT ${count || page}`;
-    `SELECT * FROM questions WHERE product_id = ${productId} AND reported = false`;
     return pool.query(queryString)
-      .then(res => { console.log('get all questions', res) })
+      .then(res => { console.log('get all questions', res.rows) })
       .catch(err => console.log('error getting all questions', err))
     // const duration = Date.now() - start
   },
--- db/schema.sql
+++ db/schema.sql
@@ -15,54 +15,69 @@ DROP TABLE IF EXISTS answers_photos;
 -- ---
 
 CREATE TABLE questions (
-  question_id SERIAL UNIQUE PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   product_id INTEGER NOT NULL,
   body VARCHAR(1000) NOT NULL,
   date_written BIGINT,
   asker_name VARCHAR(60) NOT NULL,
   asker_email VARCHAR(60) NOT NULL,
-  reported VARCHAR(10) DEFAULT 'false',
-  helpful INTEGER DEFAULT 0
+  reported BOOLEAN DEFAULT false,
+  helpful INTEGER DEFAULT 0 NOT NULL
 );
 
 COPY questions
-FROM '/Users/pmcbride/Documents/HackReactor_rfp2205/SDC/questions.csv'
+FROM '../data/questions.csv'
 DELIMITER ','
 CSV HEADER;
+
+ALTER TABLE questions RENAME COLUMN body TO question_body;
+ALTER TABLE questions RENAME COLUMN id TO question_id;
 
 -- ---
 -- Table 'answers'
 -- ---
 
 CREATE TABLE answers (
-  answer_id SERIAL UNIQUE PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   question_id INTEGER REFERENCES questions (question_id),
   body VARCHAR(1000) NOT NULL,
   date_written BIGINT,
   answerer_name VARCHAR(60) NOT NULL,
   answerer_email VARCHAR(60) NOT NULL,
-  reported INTEGER DEFAULT 0,
-  helpful INTEGER DEFAULT 0
+  reported BOOLEAN DEFAULT false,
+  helpful INTEGER DEFAULT 0 NOT NULL
 );
 
 COPY answers
-FROM '/Users/pmcbride/Documents/HackReactor_rfp2205/SDC/answers.csv'
+FROM '../data/answers.csv'
 DELIMITER ','
 CSV HEADER;
+
+ALTER TABLE answers RENAME COLUMN id TO answer_id;
 -- ---
 -- Table 'photos'
 -- ---
 
 CREATE TABLE answers_photos (
-  photo_id SERIAL UNIQUE PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   answer_id INTEGER REFERENCES answers (answer_id),
-  url VARCHAR
+  url VARCHAR(255)
 );
 
 COPY answers_photos
-FROM '/Users/pmcbride/Documents/HackReactor_rfp2205/SDC/answers_photos.csv'
+FROM '../data/answers_photos.csv'
 DELIMITER ','
 CSV HEADER;
+
+ALTER TABLE answers_photos RENAME COLUMN id TO photo_id;
+
+
+
+-- CREATE INDEXES HERE
+
+-- CREATE INDEX noreport_answers ON answers(reported) WHERE reported = false;
+
+-- CREATE INDEX photos ON answers_photos(answer_id);
 
 -- ---
 -- Test Data
