f6ddef32a13283fbd42bdba3b0822a8390cd9d32
Peter McBride <mrpetermcbride@gmail.com>
2022-07-29 23:31:24 -0700

    Updated routes and controllers files

--- db/index.js
+++ db/index.js
@@ -11,21 +11,59 @@ const pool = new Pool({
 
 module.exports = {
   getQuestions: (productId, page, count) => {
-    let queryString =
-    `SELECT * FROM questions WHERE product_id =40344 AND reported IS FALSE`;
+    let queryQuestions =
+    `SELECT * FROM questions WHERE product_id = ${productId} AND reported IS FALSE`;
     // `SELECT *
     // FROM questions
     // WHERE product_id = ${productId}
     // ORDER BY DESC
     // LIMIT ${count || page}`;
-    return pool.query(queryString)
+    return pool.query(queryQuestions)
       .then(res => { console.log('get all questions', res.rows) })
       .catch(err => console.log('error getting all questions', err))
     // const duration = Date.now() - start
   },
 
+  // const questionQueries = [several queries];
+  // const qnaData = await Promise.all(
+  //   questionQueries.map(
+  //     (query) => pool.query(productId)
+  //     )
+  // )
+
+  // let queryAnswers = `SELECT * FROM answers WHERE `
+  // let queryAnswerPhotos
+
+
   getAnswers: (questionId, page, count) => {
     console.log('working on it')
 
   },
+
+  postQuestion: () => {
+
+  },
+  // INSERT INTO for new Questions or Answers
+
+  postAnswer: () => {
+
+  },
+
+  incrementQuestionHelp: () => {
+
+  },
+//   UPDATE questions
+// SET
+
+  reportQuestion: () => {
+
+  },
+
+  incrementAnswerHelp: () => {
+
+  },
+
+  reportAnswer: () => {
+
+  }
 }
