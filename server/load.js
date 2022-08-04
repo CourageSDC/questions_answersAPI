b6a991022526cdfbb9f45f0952d379d3c234bf10
Peter McBride <mrpetermcbride@gmail.com>
2022-08-03 22:42:51 -0700

    Added k6 Stress Testing for getQuestions and getAnswers

---
+++ server/load.js
@@ -0,0 +1,59 @@
+import http from 'k6/http';
+import { sleep, check, group } from 'k6';
+
+export const options = {
+  insecureSkipTLSVerify: true,
+  noConnectionReuse: false,
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: '1000',
+      timeUnit: '1s',
+      //note: 1000 iterations per second
+      duration: '60s',
+      preAllocatedVUs: 20,
+      maxVUs: 200
+    },
+  },
+};
+
+// export let options = {
+//   insecureSkipTLSVerify: true,
+//   noConnectionReuse: false,
+//   scenarios: {
+//     constant_request_rate: {
+//       executor: 'constant-arrival-rate',
+//       rate: '1000',
+//       timeUnit: '1s',
+//       duration: '60s',
+//       preAllocatedVUs: 1000,
+//       maxVUs: 1000
+//     },
+//   }
+// };
+
+export default function () {
+  let product_id = Math.floor(Math.random() * 100000 + 900000) + 1;
+  let question_id = Math.floor(Math.random() * 100000) + 1;
+  // const url = `http://localhost:4000/qa/questions`;
+  // const params = {
+  //   headers: {
+  //     'Content-Type' : 'application/json',
+  //   },
+  // };
+
+  group('GET questions', () => {
+    const questionsTest = http.get(`http://localhost:4000/qa/questions?product_id=${product_id}`);
+    check(questionsTest, { 'is status 200': (r) => r.status === 200 });
+    sleep(1);
+  });
+
+  group('GET answers', () => {
+    const answersTest = http.get(`http://localhost:4000/qa/questions/${question_id}/answers?count=500`);
+    check(answersTest, { 'is status 200': (r) => r.status === 200 });
+    sleep(1);
+  });
+
+  // http.get(url, params);
+  // sleep(1);
+}
