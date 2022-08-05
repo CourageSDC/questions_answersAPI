0197f6b5185512b523e6eab91ec1e09bdff29f41
Peter McBride <mrpetermcbride@gmail.com>
2022-08-05 11:31:40 -0700

    Added verification for Loader.io

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ app.use(express.json());
 // })
 app.use('/qa', questionsRouter);
 
+app.get('/loaderio-1d34c548d72c17e29e6072f33d86071a', (req, res) => {
+  res.send('loaderio-1d34c548d72c17e29e6072f33d86071a');
+});
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`You are listending on port ${PORT}`)
